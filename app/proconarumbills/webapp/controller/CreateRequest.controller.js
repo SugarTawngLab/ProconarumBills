41391b2b09700cc33c16c02c2effe8c542bef94c
Tang Dien <tangvietdien0707@gmail.com>
2025-05-12 04:04:03 +0000

    error at formatter.fnEnable

--- app/proconarumbills/webapp/controller/CreateRequest.controller.js
+++ app/proconarumbills/webapp/controller/CreateRequest.controller.js
@@ -5,8 +5,33 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("ns.proconarumbills.controller.CreateRequest", {
+        onInit: function () {
+            var oData = {
+                activities: [] // Must include this array!
+            };
+        
+            var oModel = new sap.ui.model.json.JSONModel(oData);
+            this.getView().setModel(oModel, "ExpenseItems");
+        },        
+
         onPressHello: function () {
             MessageToast.show("Hello from SAPUI5!");
-        }
+        },
+        onAddRow: function () {
+            var oModel = this.getView().getModel("ExpenseItems");
+            var aItems = oModel.getProperty("/activities") || [];
+        
+            var oNewItem = {
+                description: "",
+                category: "",
+                billDate: null,
+                paymentMethod: "",
+                ammount: "",
+                remark: ""
+            };
+        
+            aItems.unshift(oNewItem);  // Add to the beginning
+            oModel.setProperty("/activities", aItems);
+        }        
     });
 });
