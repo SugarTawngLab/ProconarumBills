ef3eccc0b4f70f64cdd52abfbdbc824c9ac63d0a
Tang Dien <tangvietdien0707@gmail.com>
2025-05-14 09:44:24 +0000

    add items ui

--- app/proconarumbills/webapp/controller/CreateRequest.controller.js
+++ app/proconarumbills/webapp/controller/CreateRequest.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
     return Controller.extend("ns.proconarumbills.controller.CreateRequest", {
         onInit: function () {
             var oData = {
-                activities: [] // Must include this array!
+                items: []
             };
         
             var oModel = new sap.ui.model.json.JSONModel(oData);
@@ -17,21 +17,35 @@ sap.ui.define([
         onPressHello: function () {
             MessageToast.show("Hello from SAPUI5!");
         },
+
+        onBackToExpenseRequestList: function () {
+            const oHistory = sap.ui.core.routing.History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("ListRequest", {}, true);
+			}
+        },
+
         onAddRow: function () {
             var oModel = this.getView().getModel("ExpenseItems");
-            var aItems = oModel.getProperty("/activities") || [];
+            var aItems = oModel.getProperty("/items") || [];
         
             var oNewItem = {
                 description: "",
                 category: "",
                 billDate: null,
                 paymentMethod: "",
-                ammount: "",
+                ammount: 0,
                 remark: ""
             };
         
-            aItems.unshift(oNewItem);  // Add to the beginning
-            oModel.setProperty("/activities", aItems);
+            aItems.push(oNewItem);  // Add to the beginning
+            oModel.setProperty("/items", aItems);
+            var a = 1;
         }        
     });
 });
